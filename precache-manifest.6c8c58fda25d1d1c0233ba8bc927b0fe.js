24157ee383363f521661001f55794139bce7d2e8
Filiberto Reyes <filibertoreyes55@gmail.com>
2019-08-28 23:48:28 -0700

    Initial dist subtree commit

---
+++ precache-manifest.6c8c58fda25d1d1c0233ba8bc927b0fe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac0ab2c8300f5b3436cdebb4ce49da0d",
+    "url": "/Business-Site/img/banner.ac0ab2c8.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Business-Site/robots.txt"
+  },
+  {
+    "revision": "2c7988358a9d33c2f05e4291c947830b",
+    "url": "/Business-Site/img/pic (8).2c798835.jpg"
+  },
+  {
+    "revision": "c6620f67e6cbbc0336b5",
+    "url": "/Business-Site/js/about.76d6b80d.js"
+  },
+  {
+    "revision": "beb802996c1aecd4e13f",
+    "url": "/Business-Site/js/chunk-vendors.7ffa0152.js"
+  },
+  {
+    "revision": "c63e7f1d27192a9d2dc8",
+    "url": "/Business-Site/js/app.b4443df9.js"
+  },
+  {
+    "revision": "18978ece79aafe4df1eb47a47c6aa50e",
+    "url": "/Business-Site/img/pic (7).18978ece.jpg"
+  },
+  {
+    "revision": "a11c8c50b9e9bd05876c8b1d8e37aa8c",
+    "url": "/Business-Site/img/pic (2).a11c8c50.jpg"
+  },
+  {
+    "revision": "f844698b87fe636ce49205852d9d33e0",
+    "url": "/Business-Site/index.html"
+  },
+  {
+    "revision": "d905ee3dd3a28f1861e1426b0613625a",
+    "url": "/Business-Site/img/pic (4).d905ee3d.jpg"
+  },
+  {
+    "revision": "29479d9eacdbf2d7f6334c78ed96062c",
+    "url": "/Business-Site/img/pic (5).29479d9e.jpg"
+  },
+  {
+    "revision": "11d7dac089c902dfabe84d20759a38c3",
+    "url": "/Business-Site/img/pic (3).11d7dac0.jpg"
+  },
+  {
+    "revision": "38c8bbd4f6975c459034a4518a4d9e58",
+    "url": "/Business-Site/img/pic (9).38c8bbd4.jpg"
+  },
+  {
+    "revision": "86811762fbcbd4d8638f4bfa7cc60416",
+    "url": "/Business-Site/img/pic (6).86811762.jpg"
+  },
+  {
+    "revision": "95ccb578c7d2e8967d0ad5e9865ca014",
+    "url": "/Business-Site/img/pic (1).95ccb578.jpg"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/Business-Site/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "538b760f701fbfbc393871eab097f89c",
+    "url": "/Business-Site/img/color-paint-cans-dark.538b760f.jpg"
+  },
+  {
+    "revision": "beb802996c1aecd4e13f",
+    "url": "/Business-Site/css/chunk-vendors.66b06bb4.css"
+  }
+];
